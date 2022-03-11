47b62e69c321544efaba4d2dea0d646a3861a97f
canguejamba <canguejamba@gmail.com>
2022-03-11 22:23:57 +0100

    ngrx_store and ngrx_action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from 'src/app/auth/auth.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [BrowserModule,AppRoutingModule,AuthModule ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule, 
+    StoreModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { actionRegister } from 'src/app/auth/store/actions';
 
 @Component({
   selector: 'mc-register',
@@ -14,7 +16,7 @@ export class RegisterComponent {
 
   });
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private ngrxStore: Store) { }
 
  
 
@@ -22,5 +24,6 @@ export class RegisterComponent {
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.log('submit',this.registerForm.value, this.registerForm.valid);
+    this.ngrxStore.dispatch(actionRegister(this.registerForm.value))
   }
 }
