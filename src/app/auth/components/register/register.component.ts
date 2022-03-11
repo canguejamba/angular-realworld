54e71e110c76e2f2ee72d947dd1ac7d6af1319fb
canguejamba <canguejamba@gmail.com>
2022-03-11 21:15:03 +0100

    start register

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+   profileForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
+
+ 
+
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.log('submit',this.profileForm.value, this.profileForm.valid);
+  }
+}
