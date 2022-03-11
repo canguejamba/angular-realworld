6ee7f3b1b7fdc49c19497ec80e9dba73a0459b77
canguejamba <canguejamba@gmail.com>
2022-03-11 22:55:37 +0100

    ngrx_state adding data in interfaces

---
+++ src/app/auth/components/types/requestRegister.interface.ts
@@ -0,0 +1,9 @@
+export interface  RequestRegisterInterface{ 
+    user: {
+        email: string;
+        username: string;
+        password: string;
+    }
+
+   
+}
--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,7 +1,8 @@
 import { createAction, props } from '@ngrx/store';
 import { ActionTypes } from 'src/app/auth/store/actionTypes';
+import { RequestRegisterInterface } from 'src/app/auth/components/types/requestregister.interface';
 
 export const actionRegister = createAction(
   ActionTypes.REGISTER,
-  props<{ username: string; password: string, email: string}>()
+  props<{ request: RequestRegisterInterface}>()
 );
