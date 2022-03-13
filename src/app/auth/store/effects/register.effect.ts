7bb53fafff08ab7a015f79713ea7424a6785d7ac
canguejamba <canguejamba@gmail.com>
2022-03-13 17:54:59 +0100

    created redirect to other page

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -7,14 +7,15 @@ import {
 } from 'src/app/auth/store/actions/register.action'
 import {AuthService} from 'src/app/auth/service/auth.service'
 import {CurrentUserInterface} from 'src/app/shared/types/currentUser.interface'
-import {catchError, map, of, switchMap} from 'rxjs'
+import {catchError, map, of, switchMap, tap} from 'rxjs'
 import {HttpErrorResponse} from '@angular/common/http'
 import {LocalStoregePersistenceService} from 'src/app/shared/services/localstoragePersistence.service'
+import {Router} from '@angular/router'
 
 @Injectable()
 export class EffectRegister {
   register$ = createEffect(() =>
-    this.action$.pipe(
+    this.actions$.pipe(
       ofType(actionRegister),
       // read request
       switchMap(({request}) => {
@@ -36,9 +37,21 @@ export class EffectRegister {
     )
   )
 
+  redirectAfterSubmit = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(actionRegisterSuccess),
+        tap(() => {
+          this.router.navigateByUrl('/')
+        })
+      ),
+    {dispatch: false}
+  )
+
   constructor(
-    private action$: Actions,
+    private actions$: Actions,
     private authService: AuthService,
-    private localStoregePersistenceService: LocalStoregePersistenceService
+    private localStoregePersistenceService: LocalStoregePersistenceService,
+    private router: Router
   ) {}
 }
